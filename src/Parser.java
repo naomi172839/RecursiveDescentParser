417179b43dca32befc5e010a85aff72845dd5b69
nbonnin <nbonnin@student.umgc.edu>
2020-06-08 20:34:38 -0400

    Comments Done

--- src/Parser.java
+++ src/Parser.java
@@ -11,6 +11,7 @@
  * Imports used at various points throughout the program.
  * Concurrent lists are used because of swing multithreading.
  */
+
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
@@ -55,14 +56,14 @@ public class Parser {
             overall = overall.replace(";", " ; ");  //Place spaces around the semicolon
             overall = overall.replace(".", " . ");  //Places spaces around the period
             overall = overall.replace(",", " , ");  //Places spaces around the commas
-            overall = overall.replace("\n",""); //Removes pesky new line characters
+            overall = overall.replace("\n", ""); //Removes pesky new line characters
             split = new LinkedBlockingQueue<>(Arrays.asList(overall.split("[\\s]")));   //Splits on whitespace
             split.removeAll(Collections.singleton("")); //Removes any empty strings
             split.removeAll(Collections.singleton("\n"));   //Removes any remaining new line characters
             next(); //Loads in the first token
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(frame,e.getMessage() + " Not Found",  //Displays the error message
-                    "Parse Error",JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(frame, e.getMessage() + " Not Found",  //Displays the error message
+                    "Parse Error", JOptionPane.ERROR_MESSAGE);
             System.exit(0); //Exits the program
         }
     }
@@ -75,10 +76,10 @@ public class Parser {
     private static void next() {
         current = split.poll(); //Load in current from head of the queue
         lookahead = split.peek();   //Peak at the next token at the head of the queue
-        if(current == null) {   //Null check to prevent null pointer exception
+        if (current == null) {   //Null check to prevent null pointer exception
             return;
         }
-        if(current.equals(" ") || current.equals("")) { //To ignore  spaces and empty strings
+        if (current.equals(" ") || current.equals("")) { //To ignore  spaces and empty strings
             next(); //Recursively go through the tokens until an appropriate token is found
         }
     }
@@ -94,24 +95,24 @@ public class Parser {
      * Helper method to return the character at the tail of the string
      */
     private static char atTail(String string) {
-        return string.charAt(string.length()-1);    //Return the character at the tail
+        return string.charAt(string.length() - 1);    //Return the character at the tail
     }
 
     /*
      * Helper method to remove quotes from strings and to ensure that multi word strings are captured
      */
     private static void string() throws ParseException {
-        if(atHead(current) == '"') {    //Makes sure string is in proper format
+        if (atHead(current) == '"') {    //Makes sure string is in proper format
             current = current.substring(1); //Remove leading quote
             StringBuilder sb = new StringBuilder(); //Used to hold the string
-            while(atTail(current) != '"') { //While there is not a closing parenthesis
-                if(current == null) {   //If the end of the file is reached and there is not a closing parenthesis
+            while (atTail(current) != '"') { //While there is not a closing parenthesis
+                if (current == null) {   //If the end of the file is reached and there is not a closing parenthesis
                     error("Mismatched Quotations");
                 }
                 sb.append(current).append(" "); //Add the current token to the string builder and adds a space
                 next(); //Loads the next token
             }
-            current = current.substring(0, current.length()-1); //Removes tailing quotation
+            current = current.substring(0, current.length() - 1); //Removes tailing quotation
             sb.append(current); //Add current token to the string builder;
             current = sb.toString();    //Make current the total string;
         } else {
@@ -125,7 +126,7 @@ public class Parser {
     private static void number() throws ParseException {
         try {
             new BigInteger(current);    //Creates a new BigInteger, will throw NumberFormatException if fails
-        } catch(NumberFormatException e) {
+        } catch (NumberFormatException e) {
             error(current + " is not a number");
         }
     }
@@ -134,7 +135,7 @@ public class Parser {
      * Helper method to check for a left parenthesis, and then advance the token
      */
     private static void leftParen() throws ParseException {
-        if(current.equals("(")) {
+        if (current.equals("(")) {
             next();
         } else {
             error("Symbol must be a (, not a " + current);
@@ -145,7 +146,7 @@ public class Parser {
      * Helper method to check for a right parenthesis, will advance the next token
      */
     private static void rightParen() throws ParseException {
-        if(current.equals(")")) {
+        if (current.equals(")")) {
             next();
         } else {
             error("Symbol must be a ), not a " + current);
@@ -156,7 +157,7 @@ public class Parser {
      * Helper method to check for a comma, will advance to the next token
      */
     private static void comma() throws ParseException {
-        if(current.equals(",")) {
+        if (current.equals(",")) {
             next();
         } else {
             error("Symbol must be a ,, not a " + current);
@@ -167,7 +168,7 @@ public class Parser {
      * Helper method to check for a colon, will advance the next token
      */
     private static void colon() throws ParseException {
-        if(current.equals(":")) {
+        if (current.equals(":")) {
             next();
         } else {
             error("Symbol must be a :, not a " + current);
@@ -178,7 +179,7 @@ public class Parser {
      * Helper method to check for semi colon, will advance to the next token.
      */
     private static void semiColon() throws ParseException {
-        if(current.equals(";")) {
+        if (current.equals(";")) {
             next();
         } else {
             error("Symbol must be a ;, not a " + current);
@@ -191,7 +192,7 @@ public class Parser {
      * Will show the window to the user.
      */
     private static void period() throws ParseException {
-        if(current.equals(".")) {
+        if (current.equals(".")) {
             frame.pack();   //Makes sure everything is arrayed properly
             frame.setVisible(true);
         } else {
@@ -203,7 +204,7 @@ public class Parser {
      * Helper method to check for the keyword 'End', will advance to the next token.
      */
     private static void end() throws ParseException {
-        if(current.equals("End")) {
+        if (current.equals("End")) {
             next();
         } else {
             error("Invalid keyword: " + current);
@@ -228,7 +229,7 @@ public class Parser {
      * Should follow the progression: Window STRING '('NUMBER ',' NUMBER ')'
      */
     private static void window() throws ParseException {
-        if(current.equals("Window")) {
+        if (current.equals("Window")) {
             next(); //Should move token to a string
             string();   //Checks validity of string
             frame = new JFrame(current);    //Sets title of frame
@@ -257,7 +258,7 @@ public class Parser {
      * BNF = layout ::= Layout layout_type ':'
      */
     private static void layout() throws ParseException {
-        if(current.equals("Layout")) {  //Check to make sure proper keyword
+        if (current.equals("Layout")) {  //Check to make sure proper keyword
             next(); //Should be a layout_type
             layoutType();   //Pass control to layoutType
             colon();    //Check if a colon is present
@@ -273,7 +274,7 @@ public class Parser {
      * widgets will always be followed by the 'End' keyword
      */
     private static void widgets() throws ParseException {
-        if(!current.equals("End")) {    //Checks if there are more widgets to add
+        if (!current.equals("End")) {    //Checks if there are more widgets to add
             widget();
             widgets();
         }
@@ -307,7 +308,7 @@ public class Parser {
      * BNF = Textfield NUMBER ';'
      */
     private static void textfield() throws ParseException {
-        if(current.equals("Textfield")) {
+        if (current.equals("Textfield")) {
             next(); //Advance to next token
             number();   //Check if valid number
             int dimension1 = Integer.parseInt(current); //Convert to integer
@@ -326,7 +327,7 @@ public class Parser {
      * BNF = Panel layout widgets End ';'
      */
     private static void panel() throws ParseException {
-        if(current.equals("Panel")) {   //Keyword check
+        if (current.equals("Panel")) {   //Keyword check
             JPanel panel = new JPanel();    //Creates the panel
             toAddTo.add(panel); //Adds it to the parent object
             toAddTo = panel;    //Sets the panel as the container to add to
@@ -339,109 +340,152 @@ public class Parser {
         }
     }
 
+    /*
+     * Method for keyword 'Label'
+     * BNF = Label STRING ';'
+     */
     private static void label() throws ParseException {
-        if(current.equals("Label")) {
-            next();
-            string();
-            JLabel label = new JLabel(current);
-            toAddTo.add(label);
-            next();
-            semiColon();
+        if (current.equals("Label")) {   //Keyword check
+            next(); //Advance to next token
+            string();   //Check if valid string
+            JLabel label = new JLabel(current); //Create lanel
+            toAddTo.add(label); //Add label to the appropriate container
+            next(); //Next token
+            semiColon();    //Semicolon check
         }
     }
 
+    /*
+     * Method for keyword Group
+     * BNF = Group radio_buttons End ';'
+     */
     private static void group() throws ParseException {
-        if(current.equals("Group")) {
-            buttonsToAddTo = new ButtonGroup();
-            next();
-            radioButtons();
-            end();
-            semiColon();
+        if (current.equals("Group")) {   //Keyword check
+            buttonsToAddTo = new ButtonGroup(); //Create new buttongroup
+            next(); //Next token
+            radioButtons(); //Send control to radio buttons
+            end();  //End check
+            semiColon();    //Semicolon check
         }
     }
 
+    /*
+     * Method for the non terminal radio_buttons
+     * BNF = radio_buttons ::= radio_button radio_buttons | radio_button
+     */
     private static void radioButtons() throws ParseException {
-        if(!current.equals("End")) {
-            radioButton();
-            radioButtons();
+        if (!current.equals("End")) {    //All radio buttons will end at the end keyword
+            radioButton();  //Move control to radioButton
+            radioButtons(); //Recurse back through for additional
         }
     }
 
+    /*
+     * Method for non terminal radio_button
+     * BNF = radio_button ::= Radio STRING ';'
+     */
     private static void radioButton() throws ParseException {
-        if(current.equals("Radio")) {
-            next();
-            string();
-            JRadioButton button = new JRadioButton();
-            button.setText(current);
-            next();
-            semiColon();
-            toAddTo.add(button);
-            buttonsToAddTo.add(button);
-        }
-    }
-
-    private static void button() throws ParseException {
-        if(current.equals("Button")) {
-            next();
-            string();
-            JButton button = new JButton();
-            button.setText(current);
-            next();
-            semiColon();
-            toAddTo.add(button);
-        }
-    }
-
-    private static void layoutType() throws ParseException {
-        if(current.equals("Flow")) {
-            toAddTo.setLayout(new FlowLayout());
-            next();
-        } else if(current.equals("Grid")) {
-            GridLayout grid = new GridLayout();
-            next();
-            leftParen();
-            number();
-            int dimension1 = Integer.parseInt(current);
-            next();
-            comma();
-            int dimension2 = Integer.parseInt(current);
-            grid.setRows(dimension1);
-            grid.setColumns(dimension2);
-            if(lookahead.equals(",")) {
-                next();
-                comma();
-                int dimension3 = Integer.parseInt(current);
-                next();
-                comma();
-                int dimension4 = Integer.parseInt(current);
-                grid.setHgap(dimension3);
-                grid.setVgap(dimension4);
+        if (current.equals("Radio")) {   //Keyword check
+            next(); //Next token
+            string();   //Check for valid string
+            JRadioButton button = new JRadioButton();   //Create new button
+            button.setText(current);    //Set the buttons text
+            next(); //Next token
+            semiColon();    //Semicolon check
+            toAddTo.add(button);    //Adds button to the appropriate panel
+            if (buttonsToAddTo != null) {
+                //Note that all radio buttons must belong to a group
+                buttonsToAddTo.add(button); //Adds button to group
             }
-            next();
-            rightParen();
-            toAddTo.setLayout(grid);
         }
     }
 
+    /*
+     * Method for the keyword 'Button'
+     * BNF = Button STRING ';'
+     */
+    private static void button() throws ParseException {
+        if (current.equals("Button")) { //Keyword check
+            next(); //Next token
+            string();   //String check
+            JButton button = new JButton(); //New Button
+            button.setText(current);    //Set button text
+            next(); //Next token
+            semiColon();    //Semicolon check
+            toAddTo.add(button);    //Add button to appropriate panel
+        }
+    }
+
+    /*
+     * Method for the non terminal layout_type
+     * BNF = layout_type ::= Flow | Grid '(' NUMBER ',' NUMBER [',' NUMBER ',' NUMBER] ')'
+     */
+    private static void layoutType() throws ParseException {
+        if (current.equals("Flow")) {   //Keyword check
+            toAddTo.setLayout(new FlowLayout());    //Sets the layout to appropriate container
+            next(); //Advance the token
+        } else if (current.equals("Grid")) {    //Keyword check
+            GridLayout grid = new GridLayout(); //Creates new layout item
+            next(); //Next token
+            leftParen();    //Left parenthesis check
+            number();   //Valid number check
+            int dimension1 = Integer.parseInt(current); //Saves first dimension
+            next(); //Next token
+            comma();    //Comma check
+            number();   //Number check
+            int dimension2 = Integer.parseInt(current); //Save second dimension
+            grid.setRows(dimension1);   //Set rows to first dimension
+            grid.setColumns(dimension2);    //Sets columns to second dimension
+            if (lookahead.equals(",")) {    //Handles the optional arguments
+                next(); //Next token
+                comma();    //Comma check
+                number(); //Number check
+                int dimension3 = Integer.parseInt(current); //Save dimension
+                next(); //Next token
+                comma(); //Comma check
+                number();   //Number check
+                int dimension4 = Integer.parseInt(current); //Save dimension 4
+                grid.setHgap(dimension3);   //Set Horizontal spacing
+                grid.setVgap(dimension4);   //Set Vertical spacing
+            }
+            next(); //Next token
+            rightParen();   //Right parenthesis check
+            toAddTo.setLayout(grid); //Sets the layout to the appropriate container
+        } else {
+            error("Invalid Keyword: " + current);
+        }
+    }
+
+    /*
+     * Parse method is called to begin the parse.
+     * Will show error message for any parse errors
+     */
     public static void parse() {
         try {
-            gui();
+            gui();  //GUI is the entry non terminal
         } catch (ParseException e) {
-            JOptionPane.showMessageDialog(frame,e.getMessage(), "Parse Error",JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(frame, e.getMessage(), "Parse Error", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
 
         }
     }
 
+    /*
+     * Helper method for throwing an error.
+     * Makes code slightly more readable
+     */
+    public static void error(String message) throws ParseException {
+        throw new ParseException(message);
+    }
+
+    /*
+     * Custom exception for any errors with Parsing
+     */
     public static class ParseException extends Exception {
 
         public ParseException(String message) {
             super(message);
         }
-    }
-
-    public static void error(String message) throws ParseException {
-        throw new ParseException(message);
     }
 
 }
